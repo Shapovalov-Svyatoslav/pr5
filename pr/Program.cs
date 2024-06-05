bffb1b4ff48378e3fe750317fa495b5dc2327e73
Shapovalov-Svyatoslav <rudic207r@gmail.com>
2024-06-05 21:55:52 +0300

    add projects

---
+++ pr/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var number = Convert.ToDouble(Console.ReadLine());
+
+		if (IsTwoPow(number))
+		{
+			Console.WriteLine($"{number} є степем двійки");
+		}
+		else
+		{
+			Console.WriteLine($"{number} не є степенем двійки");
+		}
+	}
+
+	private static bool IsTwoPow(double number)
+	{
+		while (number >= 2)
+		{
+			if (number == 2)
+			{
+				return true;
+			}
+			number /= 2;
+		}
+		return false;
+	}
+}
---
+++ pr2/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.Write("Введіть число: ");
+		int limit = int.Parse(Console.ReadLine());
+
+		Console.WriteLine($"Числа Армстронга до {limit}:");
+
+		for (int i = 1; i <= limit; i++)
+		{
+			if (IsArmstrongNumber(i))
+			{
+				Console.WriteLine(i);
+			}
+		}
+	}
+
+	static bool IsArmstrongNumber(int number)
+	{
+		int sum = 0;
+		int temp = number;
+		int digits = number.ToString().Length;
+
+		while (temp > 0)
+		{
+			int digit = temp % 10;
+			sum += (int)Math.Pow(digit, digits);
+			temp /= 10;
+		}
+
+		return sum == number;
+	}
+}
